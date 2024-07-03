aeb2f96554346f649b3116d1071cac5e5936f3e4
Calvin <munenecalvn@gmail.com>
2024-07-03 09:01:42 +0300

    Add dependencies and initialize database

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 // import cors from 'cors';
 import config  from './config/config';
 import logger from './config/logger';
+import Database from './config/database';
 
 dotenv.config();
 
@@ -12,10 +13,16 @@ const app = express();
 app.use(bodyParser.json());
 // app.use(cors());
 
+// Sync the database
+const db = new Database();
+db.sequelize?.sync();
+
+// Routes
 app.get('/', (req, res) => {
     res.send('Welcome to the Journal App API! 😉👋');
 });
 
+// Start the server
 app.listen(config.port, () => {
     logger.info(`Listening to Port ${config.port} 🚀🚀`);
 });

dc02b69c07e86763922fecf7783c799f2cd7c7d2
Calvin <munenecalvn@gmail.com>
2024-07-03 17:09:38 +0300

    Add token verification middleware to routes

--- Backend/src/routes/journal.routes.ts
+++ Backend/src/routes/journal.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import journalController from '../controllers/journal.controller';
+import verifyToken from '../middlewares/verifyToken';
 
 const router = Router();
 
-router.post('/', journalController.createJournal);
-router.get('/', journalController.getAllJournals);
-router.get('/:journalId', journalController.getJournalById);
-router.get('/category/:category', journalController.getJournalsByCategory);
-router.put('/:journalId', journalController.updateJournalById);
-router.delete('/:journalId', journalController.deleteJournalById);
+router.post('/', verifyToken, journalController.createJournal);
+router.get('/', verifyToken, journalController.getAllJournals);
+router.get('/:journalId', verifyToken, journalController.getJournalById);
+router.get('/category/:category', verifyToken, journalController.getJournalsByCategory);
+router.put('/:journalId', verifyToken, journalController.updateJournalById);
+router.delete('/:journalId', verifyToken, journalController.deleteJournalById);
 
 export default router;
--- Backend/src/routes/user.routes.ts
+++ Backend/src/routes/user.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import userController from '../controllers/user.controller';
+import verifyToken from '../middlewares/verifyToken';
 
 const router = Router();
-router.get('/all', userController.getAllUsers);
-router.get('/:userId', userController.getUserById);
-router.put('/update/:userId', userController.updateUserById);
-router.delete('/delete/:userId', userController.deleteUser);
+router.get('/all', verifyToken, userController.getAllUsers);
+router.get('/:userId', verifyToken, userController.getUserById);
+router.put('/update/:userId', verifyToken, userController.updateUserById);
+router.delete('/delete/:userId', verifyToken, userController.deleteUser);
 
 export default router;

86fa3304a05c39a06f0a79aabc15ca08f35a83f0
Calvin <munenecalvn@gmail.com>
2024-07-04 11:06:02 +0300

    Refactor response object in journal controller

--- Backend/src/controllers/journal.controller.ts
+++ Backend/src/controllers/journal.controller.ts
@@ -7,7 +7,7 @@ class JournalController {
             const { title, content, category } = req.body;
             const userId = req.app.locals.user.id;
             const journal = await journalService.create(title, content, category, userId);
-            res.status(201).json({ success: true, message: "Journal Created Successfully!!!", journal });
+            res.status(201).json({ success: true, message: "Journal Created Successfully!!!", data: journal});
         } catch (error) {
             res.status(400).json({ message: 'Failed to create journal', details: error });
         }
@@ -17,7 +17,7 @@ class JournalController {
         try {
             const userId = req.app.locals.user.id;
             const journals = await journalService.getAllJournals(userId);
-            res.status(200).json({ success: true, message: "All Journals", journals });
+            res.status(200).json({ success: true, message: "All Journals", data: journals});
         } catch (error) {
             res.status(400).json({ message: 'Failed to get journals', details: error });
         }
@@ -28,7 +28,7 @@ class JournalController {
             const { journalId } = req.params;
             const userId = req.app.locals.user.id;
             const journal = await journalService.getJournalById(Number(journalId), userId);
-            res.status(200).json({ success: true, message: "Journal", journal });
+            res.status(200).json({ success: true, message: "Successfully fetched Journal", data: journal});
         } catch (error) {
             res.status(400).json({ message: 'Failed to get journal', details: error });
         }
@@ -39,7 +39,7 @@ class JournalController {
             const { category } = req.params;
             const userId = req.app.locals.user.id;
             const journals = await journalService.getJournalsByCategory(category, userId);
-            res.status(200).json({ success: true, message: "Journals by Category", journals });
+            res.status(200).json({ success: true, message: "Journals by Category", data: journals});
         } catch (error) {
             res.status(400).json({ message: 'Failed to get journals by category', details: error });
         }
@@ -50,7 +50,7 @@ class JournalController {
             const { startDate, endDate } = req.query;
             const userId = req.app.locals.user.id;
             const summary = await journalService.getSummary(userId, new Date(startDate as string), new Date(endDate as string));
-            res.status(200).json({ success: true, message: "Summary", summary });
+            res.status(200).json({ success: true, message: "Journal Summary", data: summary});
         } catch (error) {
             res.status(400).json({ message: 'Failed to get summary', details: error });
         }
@@ -62,7 +62,7 @@ class JournalController {
             const { title, content, category } = req.body;
             const userId = req.app.locals.user.id;
             const updatedJournal = await journalService.updateJournalById(Number(journalId), title, content, category, userId);
-            res.status(200).json({ success: true, message: "Journal Updated", updatedJournal });
+            res.status(200).json({ success: true, message: "Journal Updated", data: updatedJournal});
         } catch (error) {
             res.status(400).json({ message: 'Failed to update journal', details: error });
         }

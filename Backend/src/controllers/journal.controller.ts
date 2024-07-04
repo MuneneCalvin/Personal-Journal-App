1e6323988f4b95efe40bc242e688d36f64b7c7e0
Calvin <munenecalvn@gmail.com>
2024-07-04 11:30:19 +0300

    Refactor journal.controller.ts to include user object in request body

--- Backend/src/controllers/journal.controller.ts
+++ Backend/src/controllers/journal.controller.ts
@@ -4,10 +4,10 @@ import journalService from '../services/journal.service';
 class JournalController {
     public async createJournal(req: Request, res: Response) {
         try {
-            const { title, content, category } = req.body;
-            const userId = req.app.locals.user.id;
+            const { title, content, category, user } = req.body;
+            const userId = user.id;
             const journal = await journalService.create(title, content, category, userId);
-            res.status(201).json({ success: true, message: "Journal Created Successfully!!!", data: journal});
+            res.status(201).json({ success: true, message: "Journal Created Successfully!!!", data: journal });
         } catch (error) {
             res.status(400).json({ message: 'Failed to create journal', details: error });
         }
@@ -16,7 +16,7 @@ class JournalController {
     public async getAllJournals(req: Request, res: Response) {
         try {
             const journals = await journalService.getall();
-            res.status(200).json({ success: true, message: "All Journals", data: journals});
+            res.status(200).json({ success: true, message: "All Journals", data: journals });
         } catch (error) {
             res.status(400).json({ message: 'Failed to get all journals', details: error });
         }
@@ -24,9 +24,10 @@ class JournalController {
 
     public async getAllJournalsByUser(req: Request, res: Response) {
         try {
-            const userId = req.app.locals.user.id;
+            const { user } = req.body;
+            const userId = user.id;
             const journals = await journalService.getAllJournalsByUser(userId);
-            res.status(200).json({ success: true, message: "All Journals by User", data: journals});
+            res.status(200).json({ success: true, message: "All Journals by User", data: journals });
         } catch (error) {
             res.status(400).json({ message: 'Failed to get all journals by user', details: error });
         }
@@ -35,9 +36,10 @@ class JournalController {
     public async getJournalById(req: Request, res: Response) {
         try {
             const { journalId } = req.params;
-            const userId = req.app.locals.user.id;
+            const { user } = req.body;
+            const userId = user.id;
             const journal = await journalService.getJournalById(Number(journalId), userId);
-            res.status(200).json({ success: true, message: "Successfully fetched Journal", data: journal});
+            res.status(200).json({ success: true, message: "Successfully fetched Journal", data: journal });
         } catch (error) {
             res.status(400).json({ message: 'Failed to get journal', details: error });
         }
@@ -46,9 +48,10 @@ class JournalController {
     public async getJournalsByCategory(req: Request, res: Response) {
         try {
             const { category } = req.params;
-            const userId = req.app.locals.user.id;
+            const { user } = req.body;
+            const userId = user.id;
             const journals = await journalService.getJournalsByCategory(category, userId);
-            res.status(200).json({ success: true, message: "Journals by Category", data: journals});
+            res.status(200).json({ success: true, message: "Journals by Category", data: journals });
         } catch (error) {
             res.status(400).json({ message: 'Failed to get journals by category', details: error });
         }
@@ -56,10 +59,11 @@ class JournalController {
 
     public async getSummary(req: Request, res: Response) {
         try {
+            const { user } = req.body;
+            const userId = user.id;
             const { startDate, endDate } = req.query;
-            const userId = req.app.locals.user.id;
-            const summary = await journalService.getSummary(userId, new Date(startDate as string), new Date(endDate as string));
-            res.status(200).json({ success: true, message: "Journal Summary", data: summary});
+            const journals = await journalService.getSummary(userId, new Date(startDate as string), new Date(endDate as string));
+            res.status(200).json({ success: true, message: "Summary", data: journals });
         } catch (error) {
             res.status(400).json({ message: 'Failed to get summary', details: error });
         }
@@ -68,10 +72,10 @@ class JournalController {
     public async updateJournalById(req: Request, res: Response) {
         try {
             const { journalId } = req.params;
-            const { title, content, category } = req.body;
-            const userId = req.app.locals.user.id;
+            const { title, content, category, user } = req.body;
+            const userId = user.id;
             const updatedJournal = await journalService.updateJournalById(Number(journalId), title, content, category, userId);
-            res.status(200).json({ success: true, message: "Journal Updated", data: updatedJournal});
+            res.status(200).json({ success: true, message: "Journal Updated", data: updatedJournal });
         } catch (error) {
             res.status(400).json({ message: 'Failed to update journal', details: error });
         }
@@ -80,9 +84,10 @@ class JournalController {
     public async deleteJournalById(req: Request, res: Response) {
         try {
             const { journalId } = req.params;
-            const userId = req.app.locals.user.id;
+            const { user } = req.body;
+            const userId = user.id;
             const deletedJournal = await journalService.deleteJournalById(Number(journalId), userId);
-            res.status(200).json({ success: true, message: "Journal Deleted", });
+            res.status(200).json({ success: true, message: "Journal Deleted" });
         } catch (error) {
             res.status(400).json({ message: 'Failed to delete journal', details: error });
         }
